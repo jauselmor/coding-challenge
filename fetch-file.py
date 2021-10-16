5a1a6dfae0b0ae8ce04a2a8cf5bcbeaea84fdd1a
Jaime Selva Morell <31344603+jauselmor@users.noreply.github.com>
2021-10-17 00:20:50 +0200

    Called functions send_sms, send_sms and send_sms. But the first parameter set to a dummy value. Finding out how to send the right value.

--- fetch-file.py
+++ fetch-file.py
@@ -11,9 +11,17 @@ import sys
 import urllib
 from urllib import urlopen
 import ijson
-from collections import Counter
 
-#url1=sys.argv[1]
+# def send_sms(phone: str, data: dict) -> None:
+def send_sms(phone, data):
+    print("SMS sent to", phone,". Data:", data)
+#def send_email(email: str, data: dict) -> None:
+def send_email(email, data):
+    print("EMAIL sent to", email,". Data:", data)
+#def send_post(url: str, data: dict) -> None:
+def send_post(url, data):
+    print("POST sent to", url,". Data:", data)
+
 url1='https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/notifications_log.json'
 
 response1=urlopen(url1).read()
@@ -28,17 +36,20 @@ for person in people:
   for k,v in person_items:
       if k == 'type':
         if v == 'email':
-         print('send_email to:')
-         print(person_items)
+         #print('send_email to:')
+         #print(person_items)
+         send_email('alguien@example.com', person_items)
         elif v == 'post':
-         print('send_post to:')
-         print(person_items)
+         #print('send_post to:')
+         #print(person_items)
+         send_post('http://holaholitahola.com', person_items)
         elif v == 'sms':
-         print('send_sms to:')
-         print(person_items)
+         #print('send_sms to:')
+         #print(person_items)
+         send_sms('12345', person_items)
         else:
          print('no valid send type:')
-         print(person_items)
+         #print(person_items)
 
 
 #print(type(person))
