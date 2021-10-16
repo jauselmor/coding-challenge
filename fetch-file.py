09cdf15ec55e1cee3da83f38a74079e415f7e5bf
Jaime Selva Morell <31344603+jauselmor@users.noreply.github.com>
2021-10-16 23:22:48 +0200

    We know to count total people contacts in json file. We can get each contact in a dict variable (person[i]) From here we are going to process each person contact.

--- fetch-file.py
+++ fetch-file.py
@@ -11,6 +11,7 @@ import sys
 import urllib
 from urllib import urlopen
 import ijson
+from collections import Counter
 
 #url1=sys.argv[1]
 url1='https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/notifications_log.json'
@@ -18,6 +19,11 @@ url1='https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/no
 response1=urlopen(url1).read()
 
 objects=ijson.items(response1,'')
-people= (o for o in objects )
+people=(o for o in objects )
 for person in people:
- print(person[10])
+ peopleNum=len(person)
+ for i in range(peopleNum):
+  print(person[i])
+
+
+#print(type(person))
