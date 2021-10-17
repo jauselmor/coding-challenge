f270f2ab5db701f29e0ce8cdd6e371dcaa77b688
Jaime Selva Morell <31344603+jauselmor@users.noreply.github.com>
2021-10-17 05:01:50 +0200

    After some debug I've added some try except, and do better readable the code.

--- send_notifications.py
+++ send_notifications.py
@@ -11,6 +11,7 @@ import sys
 import urllib
 from urllib import urlopen
 import ijson
+from validators import email_validator, url_validator, phone_validator
 
 # def send_sms(phone: str, data: dict) -> None:
 def send_sms(phone, data):
@@ -22,24 +23,34 @@ def send_email(email, data):
 def send_post(url, data):
     print("POST sent to " + str(url),". Data:", data)
 
-def precheck_send_sms(phone, name, data):
+def precheck_send_sms(phone, name):
     # Check if:
     # - mandatory parameters are present (name, phone)
     # - phone is present and format is correct.
-    print("SMS sent to " + str(phone) , ". Data:", data)
+    if name == '':
+        return False
+    else:
+        return phone_validator(phone)
 
-def precheck_send_email(email, name, data):
+def precheck_send_email(email, name):
     # Check if:
     # - mandatory parameters are present (name, email)
     # - email is present and format is correct.
     #print("EMAIL sent to " + str(email),". Data:", data)
-    EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    return re.fullmatch(EMAIL_REGEX, email)
-def precheck_send_post(url, name, data):
+    if name == '':
+        return False
+    else:
+        return email_validator(email)
+
+def precheck_send_post(url, name):
     # Check if:
     # - mandatory parameters are present (name, url)
     # - url is present and format is correct.
-    print("SMS sent to " + str(phone) , ". Data:", data)
+    if name == '':
+        return False
+    else:
+        return url_validator(url)
+
 
 
 url1='https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/notifications_log.json'
@@ -53,20 +64,20 @@ for person in people:
  for i in range(peopleNum):
   #print(person[i])
   person_items = person[i].items()
+  url=person_items[0][1]
+  phone=person_items[1][1]
+  name=person_items[3][1]
+  email=person_items[4][1]
   for k,v in person_items:
       if k == 'type':
         if v == 'email':
-         #print('send_email to:')
-         #print(person_items[4][1])
-         send_email(person_items[4][1], person_items)
+         precheck_send_email(email, name)
+         send_email(email, person_items)
         elif v == 'post':
-         #print('send_post to:')
-         #print(person_items[0])
-         send_post(person_items[0][1], person_items)
+         precheck_send_post(url, name)
+         send_post(url, person_items)
         elif v == 'sms':
-         #print('send_sms to:')
-         #print(person_items[1])
-         #print(person_items)
+         precheck_send_sms(phone, name)
          send_sms(person_items[1][1], person_items)
         else:
          print('no valid send type:')
--- validators.py
+++ validators.py
@@ -7,26 +7,28 @@ def email_validator(email):
     #email='pepito@example.mc'
     EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     myre = re.compile(EMAIL_REGEX)
-    emailfound=myre.findall(email)
-    if email == '':
-      #print('invalid')
-      #exit()
-      return False
-    elif emailfound == []:
-      #print('invalid')
-      #print(email)
-      #exit()
-      return False
-    elif emailfound[0] == email:
-      #print('valid')
-      #print(email)
-      return True
-    else:
-      #print('invalid')
-      #print(email)
-      return False
-
-def sms_validator(phone):
+    try:
+        emailfound=myre.findall(email)
+        if email == '':
+          #print('invalid')
+          #exit()
+          return False
+        elif emailfound == []:
+          #print('invalid')
+          #print(email)
+          #exit()
+          return False
+        elif emailfound[0] == email:
+          #print('valid')
+          #print(email)
+          return True
+        else:
+          #print('invalid')
+          #print(email)
+          return False
+    except:
+        return False
+def phone_validator(phone):
 ## Too much simple but according the data provided, there are many formats of phone numbers in the list and I think is not required to lose too much time on it.
     if phone != '':
       return True
@@ -34,5 +36,8 @@ def sms_validator(phone):
       return False
 
 def url_validator(url):
-    result = urlparse(url)
-    return all([result.scheme, result.netloc])
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except:
+        return False
