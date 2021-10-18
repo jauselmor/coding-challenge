44cc3a740a98cd05ad680daac413395e81ff0801
Jaime Selva Morell <31344603+jauselmor@users.noreply.github.com>
2021-10-18 03:57:07 +0200

    - README.md improved with info about the code and the challenge. - Played with multithreading and multiprocessing. Finally I deliver a script using multiprocessing but is not working as expected, the speed of processing is not better. - I think is this the last commit because I don't have more time to improve the App.
    Thanks for the opportunity.

--- README.md
+++ README.md
@@ -2,13 +2,40 @@
 
 Basically a JSON REDO log for notifications (rest of specifications in PDF provided CI-Codingchallenge-051021-1338.pdf)
 
-First idea will be:
+### How to setup and run the App:
+Main dependencies:
+- Linux
+- Python 2.7.9
+- ijson 3.1.4
+
+````
+curl https://raw.githubusercontent.com/jauselmor/coding-challenge/main/send_notifications.py --output send_notifications.py
+curl https://raw.githubusercontent.com/jauselmor/coding-challenge/main/validators.py --output validators.py
+sudo pip install ijson
+python send_notifications.py
+````
+
+
+###Design highlevel overview::
 * Fetch JSON file (https://github.com/UN-ICC/notifications-processor/blob/master/notifications_log.json) 
 * Read contacts and identify type of the notification 
 * Identify if mandatory parameters are present and usable. We consider:
   - Valid emails match with a regex and not missing or null.
   - Phone numbers and url only invalids if missing or null.    
-* Call the right function depends on the type of notification. 
+* Call the right function depends on the type of notification.
 
-----
+### Implementation and challenge goals
+- Mandatory parameters are present and are not null: see functions, precheck_send_sms, precheck_send_phone, precheck_send_url. And file [validators](./validators.py).
+- You should write mock functions that send email, sms and post requests: Done, with a sleep to simulate the real behaviour. 
+- You need to assume that the JSON file can have thousand or million of entries: We tried to improve the performance using multiprocessing and that is why exists the file [send_notifications_multiprocesing.py](./send_notifications_multiprocesing.py) but hadn't a significant improvement (saved 5% of the time aprox.). I have no time to redesign the project, but as TO DO we could implement a solution using Celery (https://docs.celeryproject.org/en/stable/).  
+- Relevant third party libraries: Used ijson to manage big json data files.
+- Commits documented to see how the code evolve: until now 17 commits. 
+- No coding patters used, no enough experience in that field, but tried to create appropriate functions to help to read the code. Main script [send_notifications.py](./send_notifications.py) has less than 100 hundred lines. 
+- Logging. Line with message of ERROR if message can't be send because data is wrong or missing, and line with message indicating the action:
+Example:
+````
+('EMAIL sent to christopher93@example.net', '. Data:', [(u'url', u'http://fernandez.info/'), (u'phone', u'+1-152-084-6860x0958'), (u'type', u'email'), (u'name', u'Jasmine Anderson'), (u'email', u'christopher93@example.net')])
+2021-10-18 00:29:54,564 - __main__ - ERROR - [(u'url', None), (u'phone', None), (u'type', u'post'), (u'name', u'Matthew Schwartz'), (u'email', u'geoffrey45@example.net')]
+
+
  
--- send_notifications_multiprocessing.py
+++ send_notifications_multiprocessing.py
@@ -2,7 +2,7 @@
 
 ######
 # Usage:
-#       ./send_notifications.py
+#       ./send_notifications_multiprocessing.py
 #
 ######
 
@@ -14,6 +14,7 @@ import ijson
 from validators import email_validator, url_validator, phone_validator
 import logging
 from time import time, sleep
+import multiprocessing
 
 # def send_sms(phone: str, data: dict) -> None:
 def send_sms(phone, data):
@@ -56,44 +57,59 @@ def precheck_send_post(url, name):
     else:
         return url_validator(url)
 
+
+def worker(threadId, peopleNum, totalProcesses, person):
+   for i in range(threadId, peopleNum, totalProcesses):
+        person_items = person[i].items()
+        url=person_items[0][1]
+        phone=person_items[1][1]
+        name=person_items[3][1]
+        email=person_items[4][1]
+        for k,v in person_items:
+            if k == 'type':
+              if v == 'email':
+               if precheck_send_email(email, name):
+                  send_email(email, person_items)
+               else:
+                  logger.error(person_items)
+              elif v == 'post':
+               if precheck_send_post(url, name):
+                  send_post(url, person_items)
+               else:
+                  logger.error(person_items)
+              elif v == 'sms':
+               if precheck_send_sms(phone, name):
+                  send_sms(person_items[1][1], person_items)
+               else:
+                   logger.error(person_items)
+              else:
+               print('no valid send type:')
+
+##########################
+
+# For multiprocessing
+totalProcesses=8
+
+# Logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 ts = time()
 
+# Hardcoded source url to download the json
 url1='https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/notifications_log.json'
+
 response1=urlopen(url1).read()
 
 objects=ijson.items(response1,'')
 people=(o for o in objects )
 for person in people:
  peopleNum=len(person)
- for i in range(peopleNum):
-  #print(person[i])
-  person_items = person[i].items()
-  url=person_items[0][1]
-  phone=person_items[1][1]
-  name=person_items[3][1]
-  email=person_items[4][1]
-  for k,v in person_items:
-      if k == 'type':
-        if v == 'email':
-         if precheck_send_email(email, name):
-            send_email(email, person_items)
-         else:
-            logger.error(person_items)
-        elif v == 'post':
-         if precheck_send_post(url, name):
-            send_post(url, person_items)
-         else:
-            logger.error(person_items)
-        elif v == 'sms':
-         if precheck_send_sms(phone, name):
-            send_sms(person_items[1][1], person_items)
-         else:
-             logger.error(person_items)
-        else:
-         print('no valid send type:')
-         #print(person_items)
+
+processes = []
+for i in range(totalProcesses):
+    processes = multiprocessing.Process(target=worker(i, peopleNum, totalProcesses, person))
+    processes.start()
+    processes.join()
 
 logging.info('Took %s seconds', time() - ts)
 
